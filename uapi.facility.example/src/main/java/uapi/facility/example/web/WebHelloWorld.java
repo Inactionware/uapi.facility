162b9f0c0962183cdd346ab4b26e1c6dbea0fe00
minjing <min_jing@aliyun.com>
2017-08-11 16:52:33 +0800

    #4 Move code from uapi.example to uapi.facility.example project

---
+++ uapi.facility.example/src/main/java/uapi/facility/example/web/WebHelloWorld.java
@@ -0,0 +1,39 @@
+package uapi.facility.example.web;
+
+import uapi.behavior.IResponsible;
+import uapi.behavior.IResponsibleRegistry;
+import uapi.service.annotation.Inject;
+import uapi.service.annotation.OnActivate;
+import uapi.service.annotation.Service;
+import uapi.service.annotation.Tag;
+import uapi.web.http.HttpRequestEvent;
+import uapi.web.http.HttpResponseEvent;
+
+/**
+ * Created by xquan on 5/27/2017.
+ */
+@Service(autoActive = true)
+@Tag("ServletApp")
+public class WebHelloWorld {
+
+    private static final String RESP_NAME   = WebHelloWorld.class.getName();
+
+    @Inject
+    protected IResponsibleRegistry _respReg;
+
+    @OnActivate
+    public void activate() {
+        IResponsible resp = this._respReg.register(RESP_NAME);
+        resp.newBehavior("Say Hello", HttpRequestEvent.class, HttpRequestEvent.TOPIC)
+                .then((input, execCtx) -> {
+                    HttpRequestEvent event = (HttpRequestEvent) input;
+                    event.response().output("Hello from Web event!");
+                    return event;
+                })
+                .onSuccess((input, execCtx) -> {
+                    HttpRequestEvent event = (HttpRequestEvent) input;
+                    return new HttpResponseEvent(RESP_NAME, event.request(), event.response());
+                })
+                .build();
+    }
+}
